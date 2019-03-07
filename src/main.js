9eb61bec749afe31060339f23849feb43fc5b42d
Adam Lord <adam.lord@gmail.com>
2019-03-07 13:33:11 -0800

    Restore bulma

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import DefaultLayout from '~/layouts/Default.vue'
-// import '../node_modules/bulma/css/bulma.min.css'
+import '../node_modules/bulma/css/bulma.min.css'
 import '~/assets/styles.scss'
 
 export default function (Vue) {
